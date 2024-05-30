edde1235868ef47dbcc6640d7e0fd138b5ffb925
MegaHacker228 <146758274+MegaHacker228@users.noreply.github.com>
2024-05-30 23:57:48 +0300

    Add files via upload

---
+++ Connection.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEkz
+{
+    internal class Connection
+    {
+        private MySqlConnection connection;
+        private string connectionString = "Server=192.168.1.78; Port=3306; Database=makson_db; Uid=makson_db; Pwd=makson;";
+
+        public Connection()
+        {
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+
+        public void OpenConnection()
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
